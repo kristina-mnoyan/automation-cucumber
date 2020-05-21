ff9aaa8210642bfec40de3304fee7e5e9b629155
Kristina Mnoyan <kristina_mnoyan@epam.com>
2020-05-22 00:19:16 +0400

    Removed unnecessary code

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,28 +1,10 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-
-import java.util.concurrent.TimeUnit;
+import setup.DriverFactory;
 
 public class BaseTest {
 
-    protected WebDriver chromeDriver;
-
-    @BeforeClass
-    public void browserSetUp() {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--start-maximized");
-        WebDriverManager.chromedriver().setup();
-        chromeDriver = new ChromeDriver(chromeOptions);
-        chromeDriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-    }
-
     @AfterClass
     public void browserTearDown() {
-        chromeDriver.quit();
-        chromeDriver = null;
+        DriverFactory.quitDriver();
     }
 }
